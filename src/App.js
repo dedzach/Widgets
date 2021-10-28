84be0ba2874369275447e8da4cd74d166a9b1f03
Zachary Moh <zacharymoh11@gmail.com>
2021-10-28 13:10:54 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Accordion from './components/Accordion';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const items = [
+    {
+        title: 'What is React?',
+        content: 'React is a front in javascript framework'
+    },
+    {
+        title: 'What use React?',
+        content: 'Fuck this project'
+    },
+    {
+        title: 'Do you know what you are doing?',
+        content: 'Not really sir I do not!'
+    }
+]
+
+export default () => {
+    return (
+    <div>
+        <Accordion items={items} />
     </div>
-  );
-}
-
-export default App;
+    );
+};
